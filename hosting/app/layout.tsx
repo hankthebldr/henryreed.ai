8a2e7931d908f150fa3ab77f9197fd45b50f01ad
Henry Reed <Henry.reed@icloud.com>
2025-07-17 09:27:51 -0500

    import new app structure

--- hosting/app/layout.tsx
+++ hosting/app/layout.tsx
@@ -1,11 +1,74 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import "./styles/globals.scss";
+import "./styles/markdown.scss";
+import "./styles/highlight.scss";
+import { getClientConfig } from "./config/client";
+import type { Metadata, Viewport } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+// import Google Tag Manager and Google Analytics using react-gtm-module and react-ga
+import TagManager from "react-gtm-module";
+import ReactGA from "react-ga";
+import { getServerSideConfig } from "./config/server";
+
+export const metadata: Metadata = {
+  title: "Malicious GPT ",
+  description: "Are you ready to test your next generation security?",
+  appleWebApp: {
+    title: "Mal-GPT",
+    statusBarStyle: "default",
+  },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#fafafa" },
+    { media: "(prefers-color-scheme: dark)", color: "#151515" },
+  ],
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const serverConfig = getServerSideConfig();
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <meta name="config" content={JSON.stringify(getClientConfig())} />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <link
+          rel="manifest"
+          href="/site.webmanifest"
+          crossOrigin="use-credentials"
+        ></link>
+        <script src="/serviceWorkerRegister.js" defer></script>
+      </head>
+      <body>
+        {children}
+        {serverConfig?.isVercel && (
+          <>
+            <SpeedInsights />
+          </>
+        )}
+        {serverConfig?.gtmId && (
+          <>
+            {TagManager.initialize({ gtmId: serverConfig.gtmId })}
+          </>
+        )}
+        {serverConfig?.gaId && (
+          <>
+            {ReactGA.initialize(serverConfig.gaId)}
+          </>
+        )}
+      </body>
     </html>
   );
 }
--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <h1>Hello simple web front end 1.0</h1>;
+import { Analytics } from "@vercel/analytics/react";
+import { Home } from "./components/home";
+import { getServerSideConfig } from "./config/server";
+
+const serverConfig = getServerSideConfig();
+
+
+export default async function App() {
+  return (
+    <>
+      <Home />
+      {serverConfig?.isVercel && (
+        <>
+          <Analytics />
+        </>
+      )}
+    </>
+  );
 }
+
